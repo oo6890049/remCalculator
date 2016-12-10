ef3821d6a327fc63e1656b94fd96dba12aa99ef3
PeterWu <oo6890049@126.com>
2016-12-10 16:13:05 +0800

    Create remCalculator.js

---
+++ remCalculator.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+	var baseSize = 320;
+	var baseFontSize = 100;
+
+	function autoSize(width) {
+		// 如果我们设计稿为750，则传入750，否则都认为是640
+		width = width ? width : 640;
+		// 为了便于计算，在参照宽度下设html字号为100px，
+		var units = width / baseFontSize;
+
+		var clientWidth = document.documentElement.clientWidth;
+		clientWidth = clientWidth > 1080 ? 1080 : clientWidth; //设定最大值
+		clientWidth = clientWidth <= 240 ? 240 : clientWidth; //设定最小值
+
+		var calFontSize = clientWidth / units; //计算html字体的字号
+		document.documentElement.style.fontSize = calFontSize + 'px';
+	}
+
+	autoSize(baseSize);
+
+	window.onresize = function () {
+		autoSize(baseSize)
+	}
+}
